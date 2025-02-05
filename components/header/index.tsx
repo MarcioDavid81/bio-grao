806e934908a176d5d473ec915f79480e47eb15bd
marciodavid <marciodavid81@gmail.com>
2025-02-05 09:51:06 -0300

    fix: fixed error header color background at scrool

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -63,7 +63,7 @@ export default function Header() {
   return (
     <header
       className={`fixed top-0 left-0 right-0 z-50 bg-primary sm:bg-transparent transition-all duration-300
-        ${scrolled ? "bg-primary sm:rounded-br-full shadow-md transition-all" : ""}`}
+        ${scrolled ? "bg-primary sm:bg-primary sm:rounded-br-full shadow-md transition-all" : ""}`}
     >
       <div className="container flex justify-between items-center">
         <Image
