fad1655f356590925a403927feb0133629b997ad
marciodavid <marciodavid81@gmail.com>
2025-02-17 14:03:08 -0300

    fix: error wrap in mobile display on price-picker component

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
         className={`${exo.variable} antialiased`}
       >
         <Header />
-        <PriceTicker />
+        <PriceTicker name="" price="" percent="" />
         <Hero />
         {children}
         <Footer />
--- components/price-ticker/index.tsx
+++ components/price-ticker/index.tsx
@@ -1,28 +1,44 @@
 import { ArrowDown, ArrowUp } from 'lucide-react'
 import React from 'react'
 
-export default function PriceTicker() {
+const prices = [
+    {
+        name: "Soja",
+        price: "R$ 124,00",
+        percent: "-4,38%",
+    },
+    {
+        name: "Milho",
+        price: "R$ 70,00",
+        percent: "2,04%",
+    },
+    {
+        name: "Trigo",
+        price: "R$ 66,00",
+        percent: "1,23%",
+    },
+]
+
+interface Price {
+    name: string;
+    price: string;
+    percent: string;
+}
+
+export default function PriceTicker({name, price, percent}: Price) {
+
   return (
-    <div className="bg-black/80 text-white p-0 overflow-hidden mt-[72px]">
-    <div className="container flex mx-auto space-x-20  animate-scroll">
-      <div className="flex items-center gap-2">
-        <h1>Soja</h1>
-        <span className="text-md font-light text-red-600">R$ 124,00</span>
-        <span className="text-md font-light text-red-600">-4,38%</span>
-        <ArrowDown className='text-red-600' size={18} />
-      </div>
-      <div className="flex items-center gap-2">
-        <h1>Milho</h1>
-        <span className="text-md font-light text-primary">R$ 70,00</span>
-        <span className="text-md font-light text-primary">2,04%</span>
-        <ArrowUp className='text-primary' size={18} />
-      </div>
-      <div className="flex items-center gap-2">
-        <h1>Trigo</h1>
-        <span className="text-md font-light text-primary">R$ 66,00</span>
-        <span className="text-md font-light text-primary">1,23%</span>
-        <ArrowUp className='text-primary' size={18} />
-      </div>
+    <div className="bg-black/80 text-white overflow-hidden mt-[72px]">
+    <div className="container flex mx-auto space-x-20 animate-scroll">
+        {prices.map((price) => (
+            <div className="flex items-center space-x-2 whitespace-nowrap gap-2">
+                <h1>{price.name}:</h1>
+                <span className={`${price.percent.includes('-') ? 'text-red-600' : 'text-green-600'}`}>{price.price}</span>
+                <span className={`${price.percent.includes('-') ? 'text-red-600' : 'text-green-600'}`}>{price.percent}</span>
+                {price.percent.includes('-') ? <ArrowDown className='text-red-600' size={18} /> : <ArrowUp className='text-green-600' size={18} />}
+            </div>
+        ))}
+
     </div>
   </div>
   )
