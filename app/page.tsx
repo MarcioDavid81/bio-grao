bb2f549db7850d54c4684b5aa83f30349d108da7
marciodavid <marciodavid81@gmail.com>
2025-02-18 07:23:10 -0300

    fix: error commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div className='flex items-center justify-center py-10 h-screen'>
-      Home Page
+      Bem vindo à página inicial
     </div>
   )
 }
